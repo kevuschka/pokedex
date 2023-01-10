a4b964d333ea1ea321397c2cb8f9703dfe230879
Kevin Schumilo <kevuschka@outlook.com>
2023-01-10 18:11:49 +0100

    header darkmode change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let modus = 'white';
+let darkmode = 0;
 let sound = 1;
-let bgSound = 1;
+let bgSound = 0;
 let currentPageNumber = 0;
 let pokemonsPerPage = 20;
 let pokemonsPerPageMobile = 10;
@@ -9,22 +9,29 @@ let allPokemons = [];
 let favPokemons = [];
 const bgMusic = new Audio('assets/sounds/poke_theme_music.mp3');
 const select = new Audio('assets/sounds/select_sound.mp3');
+let searchResults = [];
 
 // localStorage.setItem('name', JSON.stringify(name));
 // let name = JSON.parse(localStorage.getItem(`name`)) || [];
+function darkmodeOnOff() {
+    if(darkmode) darkmode = 0;
+    else darkmode = 1;
+    localStorage.setItem('darkmode', darkmode);
+}
+
 
 async function init() {
     await getLocalStorage();
     await setLocalStorage();
     let url;
-    if(!currentPokemonList.length) {
+    if(currentPokemonList.length == 0) {
         if(window.innerWidth > 800) url = `https://pokeapi.co/api/v2/pokemon/?offset=${currentPageNumber}0&limit=${pokemonsPerPage}`;
         else url = `https://pokeapi.co/api/v2/pokemon/?offset=${currentPageNumber}0&limit=${pokemonsPerPageMobile}`
         let response = await fetch(url);
         currentPokemonList = await response.json();
         localStorage.setItem('currentPokemonList', JSON.stringify(currentPokemonList));
     }
-    if(!allPokemons.length) await preloadAllPokemons();
+    if(allPokemons.length == 0) await preloadAllPokemons();
     console.log('pokemon', currentPokemonList);
     renderHeader();
 }
@@ -37,12 +44,31 @@ function renderHeader() {
     content.innerHTML = templateHeaderTitle();
     content.innerHTML += templateHeaderFunctionalityWrapper();
     let headerFunctionality = document.getElementById(`header-functionality-wrapper`);
-    headerFunctionality.innerHTML = templateHeaderMusicIcon();
-    headerFunctionality.innerHTML += templateHeaderSoundIcon();
-    headerFunctionality.innerHTML += templateHeaderSearchbar();
-    headerFunctionality.innerHTML += templateHeaderFavorites();
-    headerFunctionality.innerHTML += templateHeaderSettings();
+    renderHeaderFunctionalityTemplates(headerFunctionality);
+    renderHeaderColor();
     addClasslist(`header`, `show-header`);
+
+}
+
+
+function renderHeaderFunctionalityTemplates(content) {
+    content.innerHTML = templateHeaderMusicIcon();
+    content.innerHTML += templateHeaderSoundIcon();
+    content.innerHTML += templateHeaderSearchbar();
+    content.innerHTML += templateHeaderFavorites();
+    content.innerHTML += templateHeaderSettings();
+    renderHeaderIcons();
+}
+
+
+function renderHeaderColor() {
+    if(darkmode) {
+        removeClasslist('header', 'bg-normal');
+        addClasslist('header', 'bg-darkmode');
+    } else {
+        removeClasslist('header', 'bg-darkmode');
+        addClasslist('header', 'bg-normal');
+    }
 }
 
 
@@ -62,68 +88,95 @@ function templateHeaderFunctionalityWrapper() {
 
 
 function templateHeaderMusicIcon() {
-    return `<img class="header-music cursor-p d-none" id="header-music" src="assets/img/music_icon.png" onclick="muteMusic()">
-            <img class="header-music cursor-p" id="header-music-mute" src="assets/img/music_icon_pause.png" onclick="unmuteMusic()">`;
+    return `<img class="header-music cursor-p d-none" id="header-music-gray" src="assets/img/music_icon.png" onclick="unmuteMusic()">
+            <img class="header-music cursor-p d-none" id="header-music-dark" src="assets/img/music_icon_dark.png" onclick="unmuteMusic()">
+            <img class="header-music cursor-p d-none" id="header-music-mute" src="assets/img/music_icon_pause.png" onclick="muteMusic()">`;
 }
 
 
 function templateHeaderSoundIcon() {
-    return `<img class="header-sound cursor-p" id="header-sound" src="assets/img/sound_icon.png" onclick="muteSound()">
+    return `<img class="header-sound cursor-p d-none" id="header-sound-gray" src="assets/img/sound_icon.png" onclick="muteSound()">
+            <img class="header-sound cursor-p d-none" id="header-sound-dark" src="assets/img/sound_icon_dark.png" onclick="muteSound()"> 
             <img class="header-sound cursor-p d-none" id="header-sound-mute" src="assets/img/sound_icon_mute.png" onclick="unmuteSound()">`;
 }
 
 
 function templateHeaderSearchbar() {
-    return `<div class="header-searchbar flex" id="header-searchbar">
+    return `<form onsubmit="" class="header-searchbar flex" id="header-searchbar">
                 <input type="text" class="header-searchbar-input w-100 h-100" id="header-searchbar-input" placeholder="Search Pokemons" onKeyUp="searchPokemon()">
+                <input type="submit" style="display: none"/>
             </input></div>`;
 }
 
 
 function templateHeaderFavorites() {
-    return `<img class="header-favorites cursor-p" id="header-favorites-gray" src="assets/img/outline_star_icon.png" onclick="markFavoritesIcon()">
+    return `<img class="header-favorites cursor-p d-none" id="header-favorites-gray" src="assets/img/outline_star_icon.png" onclick="markFavoritesIcon()">
+            <img class="header-favorites cursor-p d-none" id="header-favorites-dark" src="assets/img/outline_star_icon_dark.png" onclick="markFavoritesIcon()">
             <img class="header-favorites cursor-p d-none" id="header-favorites-white" src="assets/img/outline_star_icon_white.png" onclick="unmarkFavoritesIcon()">`;
 }
 
 
 function templateHeaderSettings() {
-    return `<img class="header-settings cursor-p" id="header-settings-gray" src="assets/img/settings_icon.png" onclick="markSettingsIcon()">
+    return `<img class="header-settings cursor-p d-none" id="header-settings-gray" src="assets/img/settings_icon.png" onclick="markSettingsIcon()">
+            <img class="header-settings cursor-p d-none" id="header-settings-dark" src="assets/img/settings_icon_dark.png" onclick="markSettingsIcon()">
             <img class="header-settings cursor-p d-none" id="header-settings-white" src="assets/img/settings_icon_white.png" onclick="unmarkSettingsIcon()">`;
 }
 
 
+function renderHeaderIcons() {
+    unmarkFavoritesIcon();
+    unmarkSettingsIcon();
+    muteMusic();
+    unmuteSound();
+}
+
+
+// function renderHeaderIconsDarkmode() {
+//     unmarkFavoritesIcon();
+//     unmarkSettingsIcon();
+//     unmuteSound();
+//     muteMusic();
+// }
+
+// function unmarkAllHeaderIcons() {
+//     unmarkFavoritesIcon();
+//     unmarkSettingsIcon();
+//     unmuteSound();
+//     muteMusic();
+// }
+
+
 function markFavoritesIcon() {
-    unmarkAllHeaderIcons();
     addClasslist(`header-favorites-gray`, `d-none`);
-    removeClasslist(`header-favorites-white`,`d-none`);
+    addClasslist(`header-favorites-dark`,`d-none`);
+    removeClasslist(`header-favorites-white`,`d-none`); 
+    darkmodeOnOff();
+    init();
 }
 
 
 function unmarkFavoritesIcon() {
     addClasslist(`header-favorites-white`,`d-none`);
-    removeClasslist(`header-favorites-gray`, `d-none`);   
+    if(darkmode) removeClasslist(`header-favorites-dark`,`d-none`);
+    else removeClasslist(`header-favorites-gray`, `d-none`);
 }
 
 
 function markSettingsIcon() {
-    unmarkAllHeaderIcons();
-    addClasslist(`header-settings-gray`, `d-none`);
-    removeClasslist(`header-settings-white`,`d-none`);
+    addClasslist(`header-settings-gray`,`d-none`);
+    addClasslist(`header-settings-dark`, `d-none`);  
+    removeClasslist(`header-settings-white`, `d-none`);
 }
 
 
 function unmarkSettingsIcon() {
     addClasslist(`header-settings-white`,`d-none`);
-    removeClasslist(`header-settings-gray`, `d-none`);   
+    if(darkmode) removeClasslist(`header-settings-dark`, `d-none`); 
+    else removeClasslist(`header-settings-gray`,`d-none`);
 }
 
 
-function unmarkAllHeaderIcons() {
-    addClasslist(`header-favorites-white`,`d-none`);
-    removeClasslist(`header-favorites-gray`, `d-none`);  
-    addClasslist(`header-settings-white`,`d-none`);
-    removeClasslist(`header-settings-gray`, `d-none`);  
-}
+
 
 
 async function loadPokemons() {
@@ -146,36 +199,39 @@ async function preloadAllPokemons() {
 
 
 function searchPokemon() {
-    let count = currentPokemonList['count'];
-    if(header-searchbar-input.value) {
-        let inputField = document.getElementById(`header-searchbar-input`);
-        inputField.value = `${header-searchbar-input.value}` + `<span class="lightgray" id="possibleSearchResult">${possibleSearchResult}</span>`;
-    }
+    let input = document.getElementById(`header-searchbar-input`).value;
+    searchResults = [];
+    if(input)
+        for (let i = 0; i < allPokemons['results'].length; i++)
+            if(allPokemons['results'][i]['name'].includes(input.toLowerCase())) 
+                searchResults.push(i);
+    else searchResults = [];
 }
 
 
 function muteMusic() {
-    addClasslist(`header-music`, `d-none`);
-    removeClasslist(`header-music-mute`, `d-none`);
+    addClasslist(`header-music-mute`, `d-none`);
+    if(darkmode) removeClasslist(`header-music-dark`, `d-none`); 
+    else removeClasslist(`header-music-gray`, `d-none`); 
     bgMusic.pause();
     bgSound = 0;
-    localStorage.setItem('bgSound', bgSound);
 }
 
 
 function unmuteMusic() {
-    removeClasslist(`header-music`, `d-none`);
-    addClasslist(`header-music-mute`, `d-none`);
+    addClasslist(`header-music-gray`, `d-none`);
+    addClasslist(`header-music-dark`, `d-none`);
+    removeClasslist(`header-music-mute`, `d-none`);
     bgMusic.volume = 0.06;
     bgMusic.loop = true;
     bgMusic.play(); 
     bgSound = 1;
-    localStorage.setItem('bgSound', bgSound);
 }
 
 
 function muteSound() {
-    addClasslist(`header-sound`, `d-none`);
+    addClasslist(`header-sound-gray`, `d-none`);
+    addClasslist(`header-sound-dark`, `d-none`);
     removeClasslist(`header-sound-mute`, `d-none`);
     sound = 0;
     localStorage.setItem('sound', sound);
@@ -183,8 +239,9 @@ function muteSound() {
 
 
 function unmuteSound() {
-    removeClasslist(`header-sound`, `d-none`);
     addClasslist(`header-sound-mute`, `d-none`);
+    if(darkmode) removeClasslist(`header-sound-dark`, `d-none`); 
+    else removeClasslist(`header-sound-gray`, `d-none`);
     sound = 1;
     localStorage.setItem('sound', sound);
 }
@@ -206,12 +263,11 @@ function removeClasslist(id, classe) {
 
 
 async function getLocalStorage() {
-    modus = localStorage.getItem('modus') || 'white';
-    sound = localStorage.getItem('sound') || 1;
-    bgSound = localStorage.getItem('bgSound') || 0;
-    currentPageNumber = localStorage.getItem('currentPageNumber') || 0;
-    pokemonsPerPage = localStorage.getItem('pokemonsPerPage') || 20;
-    pokemonsPerPageMobile = localStorage.getItem('pokemonsPerPageMobile') || 10;
+    darkmode = JSON.parse(localStorage.getItem('darkmode')) || 0;
+    sound = JSON.parse(localStorage.getItem('sound')) || 1;
+    currentPageNumber = JSON.parse(localStorage.getItem('currentPageNumber')) || 0;
+    pokemonsPerPage = JSON.parse(localStorage.getItem('pokemonsPerPage')) || 20;
+    pokemonsPerPageMobile = JSON.parse(localStorage.getItem('pokemonsPerPageMobile')) || 10;
     currentPokemonList = await JSON.parse(localStorage.getItem(`currentPokemonList`)) || [];  
     allPokemons = await JSON.parse(localStorage.getItem(`allPokemons`)) || [];
     favPokemons = await JSON.parse(localStorage.getItem(`favPokemons`)) || [];
@@ -219,8 +275,7 @@ async function getLocalStorage() {
 
 
 async function setLocalStorage() {
-    localStorage.setItem('modus', modus);
-    localStorage.setItem('bgSound', bgSound);
+    localStorage.setItem('darkmode', darkmode);
     localStorage.setItem('sound', sound);
     localStorage.setItem('currentPageNumber', currentPageNumber);
     localStorage.setItem('pokemonsPerPage', pokemonsPerPage);
