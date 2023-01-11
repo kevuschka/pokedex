b8bb3f438c1c81f34195067a88567f8e52c45c46
Kevin Schumilo <kevuschka@outlook.com>
2023-01-11 14:16:22 +0100

    pokemon element

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ let bgSound = 0;
 let currentPageNumber = 0;
 let pokemonsPerPage = 20;
 let pokemonsPerPageMobile = 10;
-let currentPokemonList = []; 
-let allPokemons = [];
+let allPokemonsPageBasic = []; 
+let allPokemonsPageElements = [];
+let allPokemonsBasic = [];
 let favPokemons = [];
 const bgMusic = new Audio('assets/sounds/poke_theme_music.mp3');
 const select = new Audio('assets/sounds/select_sound.mp3');
@@ -18,31 +19,53 @@ let searchResults = [];
 async function init() {
     await getLocalStorage();
     await setLocalStorage();
-    let url;
-    if(currentPokemonList.length == 0) {
-        if(window.innerWidth > 800) url = `https://pokeapi.co/api/v2/pokemon/?offset=${currentPageNumber}0&limit=${pokemonsPerPage}`;
-        else url = `https://pokeapi.co/api/v2/pokemon/?offset=${currentPageNumber}0&limit=${pokemonsPerPageMobile}`
-        let response = await fetch(url);
-        currentPokemonList = await response.json();
-        localStorage.setItem('currentPokemonList', JSON.stringify(currentPokemonList));
-    }
-    if(allPokemons.length == 0) await preloadAllPokemons();
-    console.log('pokemon', currentPokemonList);
+    let urlBasic;
+    let targetPageNumber = getTargetPageNumber();
+    let elementsPerPage = getPokemonsPerPageNumber();
+    if(allPokemonsPageBasic.length == 0) await getPokemonsBasicInfo(urlBasic, targetPageNumber);
+    if(allPokemonsPageElements.length == 0) await getPokemonsElementsInfo(elementsPerPage);
+    if(allPokemonsBasic.length == 0) await getAllPokemonsBasicInfo();
+    console.log('pokemon', allPokemonsPageBasic);
     renderHeader();
+    await renderPokemonsPage(elementsPerPage);
 }
 
 
-async function loadContent() {
-
+function getTargetPageNumber() {
+    if(window.innerWidth > 800) return currentPageNumber + pokemonsPerPage;
+    else return currentPageNumber + pokemonsPerPageMobile;
 }
 
 
+function getPokemonsPerPageNumber() {
+    if(window.innerWidth > 800) return pokemonsPerPage;
+    else return pokemonsPerPageMobile;
+}
 
-async function preloadAllPokemons() {
+
+async function getPokemonsBasicInfo(urlBasic, pageNumber) {
+    urlBasic = `https://pokeapi.co/api/v2/pokemon/?offset=${currentPageNumber}0&limit=${pageNumber}`;
+    let response = await fetch(urlBasic);
+    allPokemonsPageBasic = await response.json();
+    localStorage.setItem('allPokemonsPageBasic', JSON.stringify(allPokemonsPageBasic));
+}
+
+
+async function getPokemonsElementsInfo(number) {
+    allPokemonsPageElements = [];
+    for (let i = 0; i < number; i++) {
+        let url = allPokemonsPageBasic['results'][i]['url'];
+        let resp = await fetch(url);
+        allPokemonsPageElements.push(await resp.json());
+    }
+}   
+
+
+async function getAllPokemonsBasicInfo() {
     let allPokemonUrl = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1153`;
     let list = await fetch(allPokemonUrl)
-    allPokemons = await list.json();
-    localStorage.setItem(`allPokemons`, JSON.stringify(allPokemons));
+    allPokemonsBasic = await list.json();
+    localStorage.setItem(`allPokemonsBasic`, JSON.stringify(allPokemonsBasic));
 } 
 
 
@@ -63,8 +86,8 @@ async function getLocalStorage() {
     currentPageNumber = JSON.parse(localStorage.getItem('currentPageNumber')) || 0;
     pokemonsPerPage = JSON.parse(localStorage.getItem('pokemonsPerPage')) || 20;
     pokemonsPerPageMobile = JSON.parse(localStorage.getItem('pokemonsPerPageMobile')) || 10;
-    currentPokemonList = await JSON.parse(localStorage.getItem(`currentPokemonList`)) || [];  
-    allPokemons = await JSON.parse(localStorage.getItem(`allPokemons`)) || [];
+    allPokemonsPageBasic = await JSON.parse(localStorage.getItem(`allPokemonsPageBasic`)) || [];  
+    allPokemonsBasic = await JSON.parse(localStorage.getItem(`allPokemonsBasic`)) || [];
     favPokemons = await JSON.parse(localStorage.getItem(`favPokemons`)) || [];
 }
 
@@ -78,3 +101,78 @@ async function setLocalStorage() {
 }
 
 
+// POKEMON
+
+async function renderPokemonsPage(elements) {
+    let wrapper = document.getElementById(`pokemon-list-wrapper`);
+    wrapper.innerHTML = templatePokemonList();
+    let content = document.getElementById(`pokemon-list`);
+    content.innerHTML = '';
+    await renderPokemonsListContent(elements, content);
+}
+
+
+function templatePokemonList() {
+    return `<div class="pokemon-list flex w-100 h-100" id="pokemon-list"></div>`;
+}
+
+
+
+async function renderPokemonsListContent(elements, content) {
+    let name;
+    let id;
+    for (let i = 0; i < elements; i++) {
+        name = `${allPokemonsPageBasic['results'][i]['name'].charAt(0).toUpperCase()}` + `${allPokemonsPageBasic['results'][i]['name'].slice(1)}`;
+        id = allPokemonsPageElements[i]['id'];
+        content.innerHTML += templatePokemonsListElement(i, name, id);
+        renderPokemonTypes(i);
+        await renderPokemonBackgroundColor(i);
+    }
+}
+
+
+function templatePokemonsListElement(i, name, id) {
+    return `<div class="pokemon-list-element-container" id="pokemon-list-element-container-${i}">
+                <div class="pokemon-list-element flex column relative>
+                    <div class="pokemon-list-element-id-container w-100"><p>${id}</p></div>
+                    <div class="pokemon-list-element-name-container"><p>${name}</p></div>
+                    <div class="pokemon-list-element-type-container flex" id="pokemon-list-element-type-container-${i}"></div>
+                    <img src="${getPokemonImage(i)}" class="pokemon-list-element-image absolute" id="pokemon-list-element-image-${i}">
+                </div>
+            </div>`;
+}
+
+
+async function renderPokemonBackgroundColor(i) {
+    let url = allPokemonsPageElements[i]['species']['url'];
+    let resp = await fetch(url);
+    let response = await resp.json();
+    let color = response['color']['name'];
+    document.getElementById(`pokemon-list-element-container-${i}`).style.backgroundColor =`-${color}`;
+}
+
+
+function renderPokemonTypes(i) {
+    let content = document.getElementById(`pokemon-list-element-type-container-${i}`);
+    for (let j = 0; j < getPokemonTypes(i).length; j++) 
+        content.innerHTML += `<div class="pokemon-list-element-type-container flex ><p>${getPokemonTypes(i)[j]}</p></div>`;
+}
+
+
+function getPokemonTypes(i) {
+    let pokeType = [];
+    for (let j = 0; j < allPokemonsPageElements[i]['types'].length; j++) {
+        let typ = `${allPokemonsPageElements['types'][j]['type']['name'].charAt(0).toUpperCase()}` + `${allPokemonsPageElements['types'][j]['type']['name'].slice(1)}`;
+        pokeType.push(typ);
+    }
+    return pokeType;
+}
+
+
+function getPokemonImage(i) {
+    for (let j = 0; j < allPokemonsPageElements[i].length; j++) {
+        if(allPokemonsPageElements[i]['sprites']['other']['home']['front_default']) return `${allPokemonsPageElements[i]['sprites']['other']['home']['front_default']}`;
+        else if (allPokemonsPageElements[i]['sprites']['other']['official-artwork']['front_default']) return `${allPokemonsPageElements[i]['sprites']['other']['official-artwork']['front_default']}`;
+        else return `${allPokemonsPageElements[i]['sprites']['other']['dream_world']['front_default']}`;
+    }
+}
